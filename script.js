2097f556304da0b7c3ee129134001ae856bbd9ab
Gummy-168 <titikab55@gmail.com>
2024-11-28 16:04:01 +0700

    add fade in

--- script.js
+++ script.js
@@ -79,3 +79,23 @@ guestBookForm.addEventListener("submit", function (event) {
   // ล้างฟอร์มหลังจากส่งเสร็จ
   guestBookForm.reset();
 });
+
+// Fade-in
+const paragraphs = document.querySelectorAll(".fade_in");
+
+document.addEventListener("scroll", function () {
+  paragraphs.forEach((paragraphs) => {
+    if (isInviwe(paragraphs)) {
+      paragraphs.classList.add("fade_in--visible");
+    }
+  });
+});
+
+function isInviwe(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top <
+      (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
